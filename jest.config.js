9dba2577b8cdc6c4b088116f7df3ed1addf73850
Fernando Rosero <ferrosero91@gmail.com>
2025-11-14 11:44:50 -0500

    implementación de nuevas funcionalidades

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^.+\\.(css|scss|sass|less)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
